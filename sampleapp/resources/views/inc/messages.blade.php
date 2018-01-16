6eb2e0146a488b7e087f513f91838e760c9c69d9
Scot <scot.pfleghaar@gmail.com>
2018-01-15 18:45:43 -0600

    Site Complete

--- sampleapp/resources/views/inc/messages.blade.php
+++ sampleapp/resources/views/inc/messages.blade.php
@@ -3,3 +3,9 @@
     <div class="alert alert-danger">{{$error}}</div>
 @endforeach
 @endif
+
+@if(session('success'))
+    <div class="alert alert-success">
+        {{session('success')}}
+    </div>
+@endif
